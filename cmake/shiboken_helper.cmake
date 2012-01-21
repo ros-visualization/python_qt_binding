c81c8b0e24b51fca6d0fd146a2d891fbe70c28a1
Dirk Thomas <web@dirk-thomas.net>
2012-01-20 17:25:07 -0800

    added additional helper for building bindings

---
+++ cmake/shiboken_helper.cmake
@@ -0,0 +1,60 @@
+find_package(GeneratorRunner REQUIRED)
+find_package(Shiboken REQUIRED)
+find_package(PySide REQUIRED)
+find_package(PythonLibs REQUIRED)
+
+
+function(shiboken_generator PROJECT_NAME GLOBAL TYPESYSTEM WORKING_DIR GENERATED_SRCS HDRS INCLUDE_PATH BUILD_DIR)
+    # See ticket https://code.ros.org/trac/ros-pkg/ticket/5219
+    set(QT_INCLUDE_DIR_WITH_COLONS "")
+    foreach(dir ${QT_INCLUDE_DIR})
+        set(QT_INCLUDE_DIR_WITH_COLONS "${QT_INCLUDE_DIR_WITH_COLONS}:${dir}")
+    endforeach()
+
+    add_custom_command(
+        OUTPUT ${GENERATED_SRCS}
+        COMMAND ${GENERATORRUNNER_BINARY}
+            --generatorSet=shiboken
+            --include-paths=${INCLUDE_PATH}:${QT_INCLUDE_DIR_WITH_COLONS}
+            --typesystem-paths=${PYSIDE_TYPESYSTEMS}
+            --output-directory=${BUILD_DIR}
+        ${GLOBAL}
+        ${TYPESYSTEM}
+      DEPENDS ${GLOBAL} ${TYPESYSTEM} ${HDRS}
+      WORKING_DIRECTORY ${WORKING_DIR}
+      COMMENT "Running Shiboken generator for ${PROJECT_NAME} Python bindings..."
+    )
+endfunction()
+
+
+function(shiboken_include_directories PROJECT_NAME QT_COMPONENTS)
+    set(shiboken_INCLUDE_DIRECTORIES
+        ${PYTHON_INCLUDE_DIR}
+        ${SHIBOKEN_INCLUDE_DIR}
+        ${PYSIDE_INCLUDE_DIR}
+        ${PYSIDE_INCLUDE_DIR}/QtCore
+        ${PYSIDE_INCLUDE_DIR}/QtGui
+    )
+
+    foreach(component ${QT_COMPONENTS})
+        set(shiboken_INCLUDE_DIRECTORIES ${shiboken_INCLUDE_DIRECTORIES} ${PYSIDE_INCLUDE_DIR}/${component})
+    endforeach()
+
+    include_directories(${PROJECT_NAME} ${shiboken_INCLUDE_DIRECTORIES})
+endfunction()
+
+
+function(shiboken_target_link_libraries PROJECT_NAME QT_COMPONENTS)
+    set(shiboken_LINK_LIBRARIES
+        ${SHIBOKEN_PYTHON_LIBRARIES}
+        ${SHIBOKEN_LIBRARY}
+        ${PYSIDE_LIBRARY}
+    )
+ 
+    foreach(component ${QT_COMPONENTS})
+        string(TOUPPER ${component} component)
+        set(shiboken_LINK_LIBRARIES ${shiboken_LINK_LIBRARIES} ${QT_${component}_LIBRARY})
+    endforeach()
+
+    target_link_libraries(${PROJECT_NAME} ${shiboken_LINK_LIBRARIES})
+endfunction()
---
+++ cmake/sip_helper.cmake
@@ -0,0 +1,23 @@
+function(build_sip_binding PROJECT_NAME SIP_CONFIGURE SIP_FILE DEPENDED_SIPS HDRS SOURCE_DIR LIBRARY_DIR BINARY_DIR)
+    set(SIP_BUILD_DIR ${BINARY_DIR}/sip/${PROJECT_NAME})
+
+    set(INCLUDE_DIRS ${${PROJECT_NAME}_INCLUDE_DIRS})
+    set(LIBRARIES ${${PROJECT_NAME}_LIBRARIES})
+    set(LIBRARY_DIRS ${${PROJECT_NAME}_LIBRARY_DIRS})
+    set(LDFLAGS_OTHER ${${PROJECT_NAME}_LDFLAGS_OTHER})
+
+    add_custom_command(
+        OUTPUT ${SIP_BUILD_DIR}/Makefile
+        COMMAND python ${SIP_CONFIGURE} ${SIP_BUILD_DIR} ${SIP_FILE} ${LIBRARY_DIR} \"${INCLUDE_DIRS}\" \"${LIBRARIES}\" \"${LIBRARY_DIRS}\" \"${LDFLAGS_OTHER}\"
+        DEPENDS ${SIP_CONFIGURE} ${DEPENDED_SIPS} ${HDRS}
+        WORKING_DIRECTORY ${SOURCE_DIR}
+        COMMENT "Running SIP generator for ${PROJECT_NAME} Python bindings..."
+    )
+
+    add_custom_target(lib${PROJECT_NAME}.so ALL
+        COMMAND make
+        DEPENDS ${SIP_BUILD_DIR}/Makefile
+        WORKING_DIRECTORY ${SIP_BUILD_DIR}
+        COMMENT "Compiling generated code for ${PROJECT_NAME} Python bindings..."
+    )
+endfunction()
