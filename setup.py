8781685c274b53b0fda9b589d228c209cda41bdc
Dirk Thomas <dthomas@willowgarage.com>
2012-09-18 12:48:15 -0700

    minor tweaks

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
+
+from __future__ import print_function
 from distutils.core import setup
+import sys
+from xml.etree.ElementTree import ElementTree
+
+try:
+    root = ElementTree(None, 'stack.xml')
+    version = root.findtext('version')
+except Exception as e:
+    print('Could not extract version from your stack.xml:\n%s' % e, file=sys.stderr)
+    sys.exit(-1)
+
 
 setup(name='Python Qt binding',
-      version='0.2.0',
+      version=version,
       description='Qt bindings for ROS',
       packages=['python_qt_binding'],
       package_dir={'':'src'}
