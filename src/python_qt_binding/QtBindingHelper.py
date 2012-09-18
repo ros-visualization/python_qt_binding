4074d2420b6bb4594d8a07074198990a77e722a5
Dirk Thomas <dthomas@willowgarage.com>
2012-09-18 16:42:45 -0700

    fixed backward compatibility

--- src/python_qt_binding/QtBindingHelper.py
+++ src/python_qt_binding/QtBindingHelper.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+from .binding_helper import loadUi, QT_BINDING, QT_BINDING_MODULES, QT_BINDING_VERSION  # @UnusedImport
 
 print('Deprecation warning: the "python_qt_binding.QtBindingHelper" module is deprecated and will be removed in the near future.')
 print('Replace your usage of QtBindingHelper with import statements like:')
