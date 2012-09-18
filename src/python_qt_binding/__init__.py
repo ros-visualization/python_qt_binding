cd76a31abd321889adfec0c49baaab9341b984a0
Dirk Thomas <dthomas@willowgarage.com>
2012-09-18 11:16:53 -0700

    renamed internal binding helper, added deprecation warning for old usage

--- src/python_qt_binding/__init__.py
+++ src/python_qt_binding/__init__.py
@@ -11,11 +11,13 @@ All available modules are listed in QT_BINDING_MODULES.
 """
 
 import sys
-from .QtBindingHelper import loadUi, QT_BINDING, QT_BINDING_MODULES, QT_BINDING_VERSION  # @UnusedImport
+from .binding_helper import loadUi, QT_BINDING, QT_BINDING_MODULES, QT_BINDING_VERSION  # @UnusedImport
 
 # register all binding modules as sub modules of this package (python_qt_binding) for easy importing
 for module_name, module in QT_BINDING_MODULES.items():
     sys.modules[__name__ + '.' + module_name] = module
     setattr(sys.modules[__name__], module_name, module)
+    del module_name
+    del module
 
-del module_name
+del sys
