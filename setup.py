82a5b7c6facf25967b6bc2420fa065c4d9ce1498
Chris Burke <CBWhiz@gmail.com>
2013-07-16 15:02:38 -0400

    Change setup.py to be pure distutils or distribute

--- setup.py
+++ setup.py
@@ -1,11 +1,35 @@
 #!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+setup(
+    name='python_qt_binding',
+    version='0.2.10',
+    description='''This stack provides Python bindings for Qt.
+    There are two providers: pyside and pyqt.  PySide is released under
+    the LGPL.  PyQt is released under the GPL.
 
-d = generate_distutils_setup(
+    Both the bindings and tools to build bindings are included from each
+    available provider.  For PySide, it is called "Shiboken".  For PyQt,
+    this is called "SIP".
+
+    Also provided is adapter code to make the user's Python code
+    independent of which binding provider was actually used which makes
+    it very easy to switch between these.''',
+    author='Dave Hershberger, Dorian Scholz, Dirk Thomas',
     packages=['python_qt_binding'],
-    package_dir={'': 'src'}
+    package_dir={'': 'src'},
+    
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent'
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+    ]
 )
-
-setup(**d)
