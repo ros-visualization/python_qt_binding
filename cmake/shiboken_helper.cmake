0a7f68ec8cb219233c9a640ca8517af73510949d
Christoph Hellmann Santos <51296695+ipa-cmh@users.noreply.github.com>
2023-03-01 16:40:21 +0100

    Add windows support for pyside helper (#116)

--- cmake/shiboken_helper.cmake
+++ cmake/shiboken_helper.cmake
@@ -8,6 +8,31 @@ set(__PYTHON_QT_BINDING_SHIBOKEN_HELPER_INCLUDED TRUE)
 set(PYTHON_SUFFIX ".cpython-${PYTHON_VERSION_MAJOR}${PYTHON_VERSION_MINOR}m")
 set(PYTHON_EXTENSION_SUFFIX "${PYTHON_SUFFIX}-${CMAKE_CXX_LIBRARY_ARCHITECTURE}")
 
+find_package(python_qt_binding REQUIRED)
+
+macro(pyside_config option output_var)
+    if(${ARGC} GREATER 2)
+        set(is_list ${ARGV2})
+    else()
+        set(is_list "")
+    endif()
+
+    execute_process(
+      COMMAND ${Python3_EXECUTABLE} "${python_qt_binding_DIR}/pyside_config.py"
+              ${option}
+      OUTPUT_VARIABLE ${output_var}
+      OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+    if("${${output_var}}" STREQUAL "")
+        message(FATAL_ERROR "Error: Calling pyside_config.py ${option} returned no output.")
+    endif()
+    if(is_list)
+        string(REPLACE " " ";" ${output_var} "${${output_var}}")
+    endif()
+endmacro()
+
+
+
 find_package(Shiboken2 QUIET)
 if(Shiboken2_FOUND)
   message(STATUS "Found Shiboken2 version ${Shiboken2_VERSION}")
@@ -16,10 +41,22 @@ if(Shiboken2_FOUND)
     get_property(SHIBOKEN_LIBRARY TARGET Shiboken2::libshiboken PROPERTY LOCATION)
     set(SHIBOKEN_BINARY Shiboken2::shiboken2)
   endif()
-  message(STATUS "Using SHIBOKEN_INCLUDE_DIR: ${SHIBOKEN_INCLUDE_DIR}")
-  message(STATUS "Using SHIBOKEN_LIBRARY: ${SHIBOKEN_LIBRARY}")
-  message(STATUS "Using SHIBOKEN_BINARY: ${SHIBOKEN_BINARY}")
+else()
+  if(WIN32)
+    pyside_config(--shiboken-generator-path shiboken_generator_path)
+    pyside_config(--shiboken-generator-include-path shiboken_include_dir 1)
+    pyside_config(--shiboken-module-shared-libraries-cmake shiboken_shared_libraries 0)
+
+    set(SHIBOKEN_BINARY "${shiboken_generator_path}/shiboken2${CMAKE_EXECUTABLE_SUFFIX}")
+    set(SHIBOKEN_LIBRARY ${shiboken_shared_libraries})
+    set(SHIBOKEN_INCLUDE_DIR ${shiboken_include_dir})
+  endif()
 endif()
+
+message(STATUS "Using SHIBOKEN_INCLUDE_DIR: ${SHIBOKEN_INCLUDE_DIR}")
+message(STATUS "Using SHIBOKEN_LIBRARY: ${SHIBOKEN_LIBRARY}")
+message(STATUS "Using SHIBOKEN_BINARY: ${SHIBOKEN_BINARY}")
+
 set(PYTHON_BASENAME "${PYTHON_SUFFIX}-${CMAKE_CXX_LIBRARY_ARCHITECTURE}")
 
 find_package(PySide2 QUIET)
@@ -31,7 +68,16 @@ if(PySide2_FOUND)
   endif()
   message(STATUS "Using PYSIDE_INCLUDE_DIR: ${PYSIDE_INCLUDE_DIR}")
   message(STATUS "Using PYSIDE_LIBRARY: ${PYSIDE_LIBRARY}")
+else()
+  if(WIN32)
+    pyside_config(--pyside-include-path pyside_include_dir 1)
+    pyside_config(--pyside-shared-libraries-cmake pyside_shared_libraries 0)
+    set(PYSIDE_INCLUDE_DIR ${pyside_include_dir})
+    set(PYSIDE_LIBRARY ${pyside_shared_libraries})
+  endif()
 endif()
+message(STATUS "Using PYSIDE_INCLUDE_DIR: ${PYSIDE_INCLUDE_DIR}")
+message(STATUS "Using PYSIDE_LIBRARY: ${PYSIDE_LIBRARY}")
 
 set(Python_ADDITIONAL_VERSIONS "${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR}")
 find_package(PythonLibs "${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR}")
@@ -71,7 +117,8 @@ macro(_shiboken_generator_command VAR GLOBAL TYPESYSTEM INCLUDE_PATH BUILD_DIR)
     --enable-pyside-extensions
     --include-paths=${INCLUDE_PATH_WITH_COLONS}${SHIBOKEN_HELPER_INCLUDE_DIRS_WITH_COLONS}
     --typesystem-paths=${PYSIDE_TYPESYSTEMS}
-    --output-directory=${BUILD_DIR} ${GLOBAL} ${TYPESYSTEM})
+    --output-directory=${BUILD_DIR} ${GLOBAL} ${TYPESYSTEM}
+    --language-level=c++17)
 endmacro()
 
 
@@ -107,7 +154,16 @@ function(shiboken_generator PROJECT_NAME GLOBAL TYPESYSTEM WORKING_DIR GENERATED
     )
 endfunction()
 
-
+function(shiboken_generator_ext PROJECT_NAME GLOBAL TYPESYSTEM WORKING_DIR GENERATED_SRCS INCLUDE_PATH BUILD_DIR)
+    _shiboken_generator_command(COMMAND "${GLOBAL}" "${TYPESYSTEM}" "${INCLUDE_PATH}" "${BUILD_DIR}")
+    add_custom_command(
+        OUTPUT ${GENERATED_SRCS}
+        COMMAND ${COMMAND}
+      DEPENDS ${GLOBAL} ${TYPESYSTEM}
+      WORKING_DIRECTORY ${WORKING_DIR}
+      COMMENT "Running Shiboken generator for ${PROJECT_NAME} Python bindings..."
+    )
+endfunction()
 #
 # Add the Shiboken/PySide specific include directories.
 #
